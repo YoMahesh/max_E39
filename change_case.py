78f181ef346f7b249437578501b3523b047dc287
MaheshSulegai <156065935+YoMahesh@users.noreply.github.com>
2024-05-28 08:59:27 +0530

    Update change_case.py

--- change_case.py
+++ change_case.py
@@ -2,5 +2,5 @@ a:str
 aUp:str
 a = input("write a word: ")
 aUp = a.swapcase()
-print("Entered String: "+a)
-print("String after change case: "+aUp)
+print("Entered String is: "+a)
+print("String after change case is: "+aUp)
